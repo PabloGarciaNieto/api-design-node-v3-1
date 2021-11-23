2fe33a279ee6accfb1ee733bed8acec1120ebd46
pablo <pgnieto@paradigmadigital.com>
2021-11-23 19:07:13 +0100

    second exercise

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,21 @@
-export default {}
+export const allItems = (req, res) => {
+  res.send({ message: 'These are all the items' })
+}
+
+export const oneItem = (req, res) => {
+  res.send({ message: 'This is an item' })
+}
+
+export const createItem = (req, res) => {
+  res.send({ message: 'Item created' })
+}
+
+export const updateItem = (req, res) => {
+  res.send({ message: 'Item updated' })
+}
+
+export const deleteItem = (req, res) => {
+  res.send({ message: 'Item deleted' })
+}
+
+export default { allItems, oneItem, createItem, updateItem, deleteItem }
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,24 @@
 import { Router } from 'express'
+import {
+  allItems,
+  oneItem,
+  createItem,
+  updateItem,
+  deleteItem
+} from './item.controllers'
 
 const router = Router()
+// /api/item
+router
+  .route('/')
+  .get(allItems)
+  .post(createItem)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .put(updateItem)
+  .delete(deleteItem)
+  .get(oneItem)
 
 export default router
