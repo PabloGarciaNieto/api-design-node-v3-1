24c256d48814d0d539ecb7b2f104882e5620f831
pablo <pgnieto@paradigmadigital.com>
2021-11-24 12:53:45 +0100

    fourth exercise

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -2,3 +2,9 @@ import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
 
 export default crudControllers(Item)
+
+/* If you wanto to override any generic controller for the model here imported
+  export default {
+  ...crudControllers(Item),
+  getOne() {new code to override the generic controller}
+} */
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,62 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
 
-export const getMany = model => async (req, res) => {}
+  const document = await model.findOne({ _id: id, createdBy: userId })
 
-export const createOne = model => async (req, res) => {}
+  if (!document) {
+    return res.status(400).end()
+  }
 
-export const updateOne = model => async (req, res) => {}
+  res.status(200).json({ data: document })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
+
+  const documents = await model.find({ createdBy: userId })
+
+  res.status(200).json({ data: documents })
+}
+
+export const createOne = model => async (req, res) => {
+  const document = await model.create({ ...req.body, createdBy: req.user._id })
+
+  res.status(201).json({ data: document })
+}
+
+export const updateOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const document = await model.findOneAndUpdate(
+    { _id: id, createdBy: userId },
+    req.body,
+    { new: true }
+  )
+
+  if (!document) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: document })
+}
+
+export const removeOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const document = await model
+    .findOneAndRemove({
+      _id: id,
+      createdBy: userId
+    })
+    .exec()
+
+  if (!document) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: document })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
